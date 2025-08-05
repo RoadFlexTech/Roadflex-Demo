21ba418c933f1bab99a5ad7bd1644f070c43003f
Greg <greg@roadflex.com>
2025-08-05 04:55:05 +0000

    demo commit

---
+++ my-node-app/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+app.get('/', (req, res) => {
+  res.send('Hello from Cloud Run version 2!');
+});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
