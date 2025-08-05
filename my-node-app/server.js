56e6f6a9d9a56b72afe20dab140362a4b2b76bd9
Greg <greg@roadflex.com>
2025-08-05 05:17:34 +0000

    server.js

--- my-node-app/server.js
+++ my-node-app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 app.get('/', (req, res) => {
-  res.send('Hello from Cloud Run version 2!');
+  res.send('Hello from Cloud Run version 3 on 5th August!');
 });
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
